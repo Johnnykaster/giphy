d8b8fb1bce540992375a618fec78983486b3ff2e
JohnnyKaster <johnnykaster@rocketmail.com>
2020-03-25 01:34:11 +0000

    almost done, just need to figure out how to append the generated image url

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    let userInput = $("#search-term").val();
+    let requestLink = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    console.log(requestLink);
+    fetch(requestLink)
+        .then(function(request){
+            console.log(request);
+            return request.json();
+        })
+        .then(function(object){
+            console.log(object.data.length);
+            var randomNum = Math.round(Math.random() * object.data.length)
+            console.log(randomNum);
+            console.log(object);
+            var pic_url = object.data[randomNum].url;
+            console.log(pic_url);
+        })
+        $("body").append("<img src='" + pic_url + "'>");
 });
 
